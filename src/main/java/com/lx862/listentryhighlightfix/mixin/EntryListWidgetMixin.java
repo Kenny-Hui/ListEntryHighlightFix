ab48e40ce5056d5f02c1acb6f0accacf888ff2f1
LX862 <kenny.mh.hui@outlook.com>
2024-08-25 15:45:33 +0800

    Bump dependencies

--- src/main/java/com/lx862/listentryhighlightfix/mixin/EntryListWidgetMixin.java
+++ src/main/java/com/lx862/listentryhighlightfix/mixin/EntryListWidgetMixin.java
@@ -1,9 +1,7 @@
 package com.lx862.listentryhighlightfix.mixin;
 
 import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.gui.widget.ClickableWidget;
 import net.minecraft.client.gui.widget.EntryListWidget;
-import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,13 +9,13 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(EntryListWidget.class)
-public abstract class EntryListWidgetMixin extends ClickableWidget {
+public abstract class EntryListWidgetMixin {
 
     @Shadow public abstract int getRowLeft();
 
     @Shadow public abstract int getRowRight();
-    public EntryListWidgetMixin(int x, int y, int width, int height, Text message) {
-        super(x, y, width, height, message);
+    public EntryListWidgetMixin() {
+        super();
     }
 
     @Inject(method = "drawSelectionHighlight", at = @At("HEAD"), cancellable = true)
